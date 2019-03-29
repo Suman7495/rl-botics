055e7cc2f7c3eebd729bf5c2a44ee06b8cdf1eee
Suman Pal <suman7495@gmail.com>
2019-03-29 11:17:16 +0100

    Modified qlearning

--- rl-botics/qlearning/main.py
+++ rl-botics/qlearning/main.py
@@ -1,6 +1,20 @@
 import argparse
-import gym
-from TabularQLearning import *
+from qlearning import *
+import hyperparameters as h
+
+
+def argparser():
+    """
+    Input argument parser.
+    Loads default hyperparameters from hyperparameters.py
+    :return: Parsed arguments
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--env', type=str, default=h.env_name)
+    parser.add_argument('--gamma', type=float, default=h.gamma)
+    parser.add_argument('--lr', type=float, default=h.lr)
+    parser.add_argument('--num_episodes', type=int, default=h.num_ep)
+    return parser.parse_args()
 
 
 def main():
@@ -8,18 +22,13 @@ def main():
         Main script.
         Default environment: FrozenLake-v0
     """
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--env', type=str, default='FrozenLake-v0')
-    parser.add_argument('--gamma', type=float, default=0.95)
-    parser.add_argument('--lr', type=float, default=0.8)
-    parser.add_argument('--num_episodes', type=int, default=1e3)
-    args = parser.parse_args()
-
+    args = argparser()
     agent = TabularQLearning(args)
     print("Training agent...\n")
     agent.train()
     print("Training completed successfully.\nPrinting Results.\n")
     agent.print_results()
 
+
 if __name__ == "__main__":
-	main()
+    main()
